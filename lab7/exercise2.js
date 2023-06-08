3771f10d9fdeb528ffb764cfcc02ec4ee2b26336
Tin Tun Aung <tintunaung.tta.1993@gmail.com>
2023-06-08 12:50:01 -0500

    Modify Lab7:

--- lab7/exercise2.js
+++ lab7/exercise2.js
@@ -9,28 +9,3 @@ let group = {
     }
 };
 group.showList();
-
-//Call and Apply
-// let group = {
-//     title: "Our Group",
-//     students: ["John", "Pete", "Alice"],
-//     showList: function() {
-//         this.students.forEach(function(student) {
-//             console.log(this.title + ": " + student);
-//         }, this); 
-//     }
-// };
-// group.showList();
-
-//Self
-// let group = {
-//     title: "Our Group",
-//     students: ["John", "Pete", "Alice"],
-//     showList: function() {
-//         let self = this;
-//         this.students.forEach(function(student) {
-//             console.log(self.title + ": " + student);
-//         }); 
-//     }
-// };
-// group.showList();
